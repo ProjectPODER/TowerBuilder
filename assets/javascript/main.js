e33f45dead89a83b3f837c9c0fceec98c675ec2a
Marisol Carrillo <marisol@rindecuentas.org>
2019-03-25 18:01:30 -0600

    Adding dropdown and responsive navbar

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,3 +10,58 @@ $(function() {
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+
+var autocollapse = function (menu,maxHeight) {
+    
+    var nav = $(menu);
+    var navHeight = nav.innerHeight();
+    if (navHeight >= maxHeight) {
+        
+        $(menu + ' .dropdown').removeClass('d-none');
+        $(".navbar-nav").removeClass('w-auto').addClass("w-100");
+        
+        while (navHeight > maxHeight) {
+            //  add child to dropdown
+            var children = nav.children(menu + ' li:not(:last-child)');
+            var count = children.length;
+            $(children[count - 1]).prependTo(menu + ' .dropdown-menu');
+            navHeight = nav.innerHeight();
+        }
+        $(".navbar-nav").addClass("w-auto").removeClass('w-100');
+        
+    }
+    else {
+        
+        var collapsed = $(menu + ' .dropdown-menu').children(menu + ' li');
+      
+        if (collapsed.length===0) {
+          $(menu + ' .dropdown').addClass('d-none');
+        }
+      
+        while (navHeight < maxHeight && (nav.children(menu + ' li').length > 0) && collapsed.length > 0) {
+            //  remove child from dropdown
+            collapsed = $(menu + ' .dropdown-menu').children('li');
+            $(collapsed[0]).insertBefore(nav.children(menu + ' li:last-child'));
+            navHeight = nav.innerHeight();
+        }
+
+        if (navHeight > maxHeight) { 
+            autocollapse(menu,maxHeight);
+        }
+        
+    }
+};
+
+
+$(document).ready(function () {
+
+    // when the page loads
+    autocollapse('#nav',50); 
+    
+    // when the window is resized
+    $(window).on('resize', function () {
+        autocollapse('#nav',50); 
+    });
+
+});
