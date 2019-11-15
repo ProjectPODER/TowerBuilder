7f869c25eaeb4c63636d2453fec49fd06365aa15
Marisol Carrillo <marisol@rindecuentas.org>
2019-11-15 16:19:06 -0600

    Fix contracts search number

--- assets/javascript/towersearch.js
+++ assets/javascript/towersearch.js
@@ -33,7 +33,8 @@ function initSearch() {
                 title: "{{site.column_3}}",
                 data: "contracts_amount",
                 type: "num",
-                render: function(data, type, row, meta) { return data.toFixed(2) },
+                // render: function(data, type, row, meta) { return data.toFixed(2) },
+                render: $.fn.dataTable.render.number( ',', '.', 2, '$' ),
                 searchable: "false"
             }
         ],
